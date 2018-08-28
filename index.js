7751ddcd1fbabd612858f45f146fbe1af3a941df
AlfathDirk <alfathdirk@gmail.com>
2018-08-28 12:23:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ class Mysql extends Connection {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       this.createConnection();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
+      return;
+    } 
+    throw err;                                  // server variable configures this)
   }
 
   _mysqlQuery (sql, params) {
